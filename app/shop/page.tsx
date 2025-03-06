c8fead2681406b405a409ab4a4f305f3a949ac7c
Jabedweb <jabedhasan231@gmail.com>
2025-03-06 23:40:36 +0600

    Add Shop page and Shop_grid component for product display

---
+++ app/shop/page.tsx
@@ -0,0 +1,12 @@
+import Shop_grid from '@/components/Shop/Shop_grid'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Shop_grid />
+    </div>
+  )
+}
+
+export default page
---
+++ components/Shop/Shop_grid.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shop_grid = () => {
+  return (
+    <div>Shop_grid</div>
+  )
+}
+
+export default Shop_grid
