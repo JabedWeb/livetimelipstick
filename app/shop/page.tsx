af055c3ac645916ed09278cff09e19b27732ea9b
Jabedweb <jabedhasan231@gmail.com>
2025-04-07 21:28:48 +0600

    Add sunglasses component and image for virtual try-on feature

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,10 +1,12 @@
 
 import FaceLandmarkerComponent from '@/components/DontTouch/DefaultFaceLandmark'
+import Sunglasses from '@/components/DontTouch/Sunglasses'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <Sunglasses />
       <FaceLandmarkerComponent />
     </div>
   )
