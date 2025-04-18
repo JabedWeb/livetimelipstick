c0afe32d641eb497035f75fb9ec70b9cfe5a56a6
Jabedweb <jabedhasan231@gmail.com>
2025-04-19 01:56:57 +0600

    Remove LaundryBooking and unused component imports from Home page for cleaner code structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import LaundryBooking from "@/components/Booking/LaundryBooking";
+
 import ProductGrid from "@/components/Grid";
-import ProductPage from "@/components/ProductPage";
 import { LipstickProvider } from "@/context/ColorContext";
 
 
 export default function Home() {
   return (
     <div>
-      {/* <FaceLandmarkerComponent /> */}
-      {/* <ProductPage /> */}
-      <LaundryBooking />
       <LipstickProvider>
            <ProductGrid />
       </LipstickProvider>
